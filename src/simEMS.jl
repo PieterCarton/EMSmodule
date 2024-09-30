1290e355efd5e8eec051933c73574ad3a8302bdc
d.a.slaifstein@tudelft.nl <d.a.slaifstein@tudelft.nl>
2024-09-30 10:51:57 +0200

    Fixed typo

--- src/simEMS.jl
+++ src/simEMS.jl
@@ -495,14 +495,15 @@ function simTransitionFun!(results::Dict, data::Dict, s::modelSettings; typeOpt:
     # re-balance the power
     if typeOpt == "MPC"
         # from the Thermal balance we adjust the HP
-        results["Phpe"][shift] =  copy(Plt - Pst - results["Ptess"]) / data["HP"].η
         # since the TESS overcharge might have come from the ST or the HP this new HP power might be negative,
         # thus we have to check if the HP power goes negative and dump it in the house
         # excess heat, rejected from the TESS
-        if results["Phpe"][shift] < 0.
-            Qex = copy(-results["Phpe"][shift+1]) .* data["HP"].η
+        if (Plt - Pst - results["Ptess"][shift]) < 0.
+            Qex = copy(-Plt + Pst + results["Ptess"][shift]) .* data["HP"].η
             results["Phpe"][shift] = 0.; # set the HP power to 0
             results["Plt"][shift] = copy(Plt .+ Qex); # dump the rejected heat in the house
+        else
+            results["Phpe"][shift] =  copy(Plt - Pst - results["Ptess"][shift]) / data["HP"].η
         end
         # electrical re-balance with the new HP power
         # re-extract the power of the sa (just in case we have hit the SoC limits)

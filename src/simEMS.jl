f1369fcc765a3bf070eccc869dab751b2ebe49cd
d.a.slaifstein@tudelft.nl <d.a.slaifstein@tudelft.nl>
2025-03-17 13:04:38 +0100

    fix simEMS for PsaOpt

--- src/simEMS.jl
+++ src/simEMS.jl
@@ -325,11 +325,8 @@ function simulate_storage_asset!(stgAsset::BESSData, results::Dict, key::String;
     # update the results dictionary with PsaOpt
     if check
         PsaOpt = 1e-3*PsaOpt * stgAsset.GenInfo.Ns * stgAsset.GenInfo.Np; # pack power in kW
-        if typeOpt == "MPC"
-            results["P$key"][shift+1] = copy(PsaOpt[1:upSampRatio:end]); # save the downsampled array
-        else # typeOpt == "day-ahead"
-            results["P$key"] = copy(PsaOpt[1:upSampRatio:end]); # save the downsampled array
-        end
+        length(PsaOpt[1:upSampRatio:end]) == 1 ? results["P$key"][shift+1] = copy(PsaOpt[1:upSampRatio:end]) : 
+                                                results["P$key"] = copy(PsaOpt[1:upSampRatio:end]);
     end
 
     # Update the results dictionary with the Performance Vars

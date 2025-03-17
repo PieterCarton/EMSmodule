35bdd6200016be73b48b2eec89c554f5d66647e8
d.a.slaifstein@tudelft.nl <d.a.slaifstein@tudelft.nl>
2025-03-17 18:18:41 +0100

    fix

--- src/simEMS.jl
+++ src/simEMS.jl
@@ -325,7 +325,8 @@ function simulate_storage_asset!(stgAsset::BESSData, results::Dict, key::String;
     # update the results dictionary with PsaOpt
     if check
         PsaOpt = 1e-3*PsaOpt * stgAsset.GenInfo.Ns * stgAsset.GenInfo.Np; # pack power in kW
-        length(PsaOpt[1:upSampRatio:end]) == 1 ? results["P$key"][shift+1] = copy(PsaOpt[1:upSampRatio:end][:]) : 
+        println(PsaOpt[1:upSampRatio:end])
+        length(PsaOpt[1:upSampRatio:end]) == 1 ? results["P$key"][shift+1] = copy(PsaOpt[1:upSampRatio:end][1]) : 
                                                 results["P$key"] = copy(PsaOpt[1:upSampRatio:end]);
     end
 
